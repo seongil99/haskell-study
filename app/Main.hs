2d549c6db2ab00aa3dc0ce5d4bd6b9eb6f8ae91a
seongil99 <books13456@gmail.com>
2025-03-13 22:54:04 +0900

    lesson 1

--- app/Main.hs
+++ app/Main.hs
@@ -11,6 +11,18 @@ hailstoneSeq :: Integer -> [Integer]
 hailstoneSeq 1 = [1]
 hailstoneSeq n = n : hailstoneSeq (hailstone n)
 
+-- Compute the length of a list of Integers.
+intListLength :: [Integer] -> Integer
+intListLength [] = 0
+intListLength (_:xs) = 1 + intListLength xs
+
+sumEveryTwo :: [Integer] -> [Integer]
+sumEveryTwo [] = []         -- Do nothing to the empty list
+sumEveryTwo [x] = [x]       -- Do nothing to lists with a single element
+sumEveryTwo (x:(y:zs)) = (x + y) : sumEveryTwo zs
+
 main = do
   print (hailstoneSeq 5)
   print (hailstoneSeq 6)
+  print (intListLength [1, 2, 3, 4, 5])
+  print (sumEveryTwo [1, 2, 3, 4, 5])
