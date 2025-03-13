3243a45069e537c42c228f9706f3a4d7b559ce32
seongil99 <books13456@gmail.com>
2025-03-13 22:47:57 +0900

    lesson 1

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,16 @@
 module Main where
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+
+hailstone :: Integer -> Integer
+hailstone n
+  | even n = n `div` 2
+  | otherwise = 3 * n + 1
+
+hailstoneSeq :: Integer -> [Integer]
+hailstoneSeq 1 = [1]
+hailstoneSeq n = n : hailstoneSeq (hailstone n)
+
+main = do
+  print (hailstoneSeq 5)
+  print (hailstoneSeq 6)
